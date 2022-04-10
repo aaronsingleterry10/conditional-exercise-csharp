31375c898463a43803058bcaef08bc9754ecc600
Aaron Singleterry <aaron.singleterry@claritymis.com>
2022-04-10 18:22:16 -0500

    created Number validation class

---
+++ NumberValidation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalExercise
+{
+    class NumberValidation
+    {
+        public static string validation(int num)
+        {
+            if (num >= 1 && num <=10)
+            {
+                return "Valid";
+            }
+            else
+            {
+                return "Invalid";
+            }
+        }
+    }
+}
