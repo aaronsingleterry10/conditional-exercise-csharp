1be9a45c1026fe7815e360e4940b2fb2d4fc5c04
Aaron Singleterry <aaron.singleterry@claritymis.com>
2022-04-10 18:22:34 -0500

    created user number class

---
+++ UserNumber.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalExercise
+{
+    class UserNumber
+    {
+        public int userNumber;
+
+    }
+}
