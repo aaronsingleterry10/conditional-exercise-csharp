f9830db78962a1734c66ff29bed6d936146f6a47
Aaron Singleterry <aaron.singleterry@claritymis.com>
2022-04-10 22:50:47 -0500

    completed all exercises

--- Program.cs
+++ Program.cs
@@ -10,14 +10,16 @@ namespace ConditionalExercise
     {
         static void Main(string[] args)
         {
-            UserNumber user1 = new UserNumber() { userNumber = 8 };
+            //Console.Write("Type a number between 1-10: ");
+            //int userInput1 = Int32.Parse(Console.ReadLine());
+            //UserNumber user1 = new UserNumber() { userNumber = userInput1 };
             UserNumber user2 = new UserNumber() { userNumber = 2 };
             UserNumber user3 = new UserNumber() { userNumber = 0 };
             UserNumber user4 = new UserNumber() { userNumber = 10 };
             UserNumber user5 = new UserNumber() { userNumber = -3 };
             UserNumber user6 = new UserNumber() { userNumber = 23 };
 
-            var response1 = NumberValidation.validation(user1.userNumber);
+            //var response1 = NumberValidation.validation(user1.userNumber);
             var response2 = NumberValidation.validation(user2.userNumber);
             var response3 = NumberValidation.validation(user3.userNumber);
             var response4 = NumberValidation.validation(user4.userNumber);
@@ -31,25 +33,64 @@ namespace ConditionalExercise
             //Console.WriteLine("Response 5: " + response5);
             //Console.WriteLine("Response 6: " + response6);
 
-            var maxNumber1 = MaxNumber.maxNumber(user1.userNumber, user2.userNumber);
+            //Console.WriteLine("Enter two different numbers");
+            //Console.Write("First number: ");
+            //var num1 = Int32.Parse(Console.ReadLine());
+            //Console.Write("Second number: ");
+            //var num2 = Int32.Parse(Console.ReadLine());
+
+            //var maxNumber1 = MaxNumber.maxNumber(num1, num2);
             var maxNumber2 = MaxNumber.maxNumber(user3.userNumber, user4.userNumber);
             var maxNumber3 = MaxNumber.maxNumber(user5.userNumber, user6.userNumber);
 
             //Console.WriteLine("The winner is: " + maxNumber1);
             //Console.WriteLine("The winner is: " + maxNumber2);
             //Console.WriteLine("The winner is: " + maxNumber3);
-
-            var img1 = new ImageDimension() { width = 200, height = 400 };
+            //Console.Write("Enter image width: ");
+            //var imgWidth = Int32.Parse(Console.ReadLine());
+            //Console.Write("Enter image height: ");
+            //var imgHeight = Int32.Parse(Console.ReadLine());
+            //var img1 = new ImageDimension() { width = imgWidth, height = imgHeight };
             var img2 = new ImageDimension() { width = 210, height = 100 };
             var img3 = new ImageDimension() { width = 500, height = 500 };
 
-            var imgOrientation1 = img1.pageOrientation();
+            //var imgOrientation1 = img1.pageOrientation();
             var imgOrientation2 = img2.pageOrientation();
             var imgOrientation3 = img3.pageOrientation();
 
             //Console.WriteLine(imgOrientation1);
-            Console.WriteLine(imgOrientation2);
-            Console.WriteLine(imgOrientation3);
+            //Console.WriteLine(imgOrientation2);
+            //Console.WriteLine(imgOrientation3);
+            Console.Write("Enter speed limit: ");
+            int speedLimitSign = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter the speed of the car: ");
+            var speed = Int32.Parse(Console.ReadLine());
+            Car mustang = new Car() { carSpeed = 45 };
+            Car challenger = new Car() { carSpeed = 50 };
+            Car charger = new Car() { carSpeed = 55 };
+            Car viper = new Car() { carSpeed = 60 };
+            Car corvette = new Car() { carSpeed = 75 };
+            Car focus = new Car() { carSpeed = 90 };
+            Car neon = new Car() { carSpeed = 107 };
+            Car jeep = new Car() { carSpeed = 112 };
+
+            string speedCheck = SpeedLimit.speedCamera(speedLimitSign, speed);
+            Console.WriteLine(speedCheck);
+            string speedCheck2 = SpeedLimit.speedCamera(speedLimitSign, challenger.carSpeed);
+            //Console.WriteLine(speedCheck2);
+            string speedCheck3 = SpeedLimit.speedCamera(speedLimitSign, charger.carSpeed);
+            //Console.WriteLine(speedCheck3);
+            string speedCheck4 = SpeedLimit.speedCamera(speedLimitSign, corvette.carSpeed);
+            //Console.WriteLine(speedCheck4);
+            string speedCheck5 = SpeedLimit.speedCamera(speedLimitSign, neon.carSpeed);
+            string speedCheck6 = SpeedLimit.speedCamera(speedLimitSign, jeep.carSpeed);
+            //Console.WriteLine(speedCheck5);
+            //Console.WriteLine(speedCheck6);
+
+            //Console.Write("Enter a number between 1 to 10: ");
+            var input = Console.ReadLine();
+            //Console.WriteLine("you typed: " + input);
+
         }
     }
 }
