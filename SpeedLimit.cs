cff10666c2a2242c838a9597833a815abac7c392
Aaron Singleterry <aaron.singleterry@claritymis.com>
2022-04-10 22:02:23 -0500

    added speed camera method

--- SpeedLimit.cs
+++ SpeedLimit.cs
@@ -8,5 +8,22 @@ namespace ConditionalExercise
 {
     class SpeedLimit
     {
+        public static string speedCamera(int speedLimit, int carSpeed)
+        {
+            int howMuchOverLimit = (carSpeed - speedLimit);
+            int demerit = (howMuchOverLimit / 5);
+            if (howMuchOverLimit <= 0)
+            {
+                return "Ok";
+            }
+            else if (howMuchOverLimit > 60)
+            {
+                return "License Suspended";
+            }
+            else
+            {
+                return "You have " + demerit + " demerits.";
+            }
+        }
     }
 }
