e023c5a07078c289693d8f20d6498bfdd7aa0b71
Aaron Singleterry <aaron.singleterry@claritymis.com>
2022-04-10 19:06:33 -0500

    created image dimension class

---
+++ ImageDimension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalExercise
+{
+    class ImageDimension
+    {
+        public int width;
+        public int height;
+
+        public string pageOrientation()
+        {
+            if (this.height >= this.width)
+            {
+                return "Portrait";
+            }
+            else
+            {
+                return "Landscape";
+            }
+        }
+    }
+}
